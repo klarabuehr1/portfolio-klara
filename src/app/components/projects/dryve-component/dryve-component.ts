b26ce2d2fc92dbd7d9e6de4d179a190880538996
Klara Bühr <klara.buehr@hfg-gmuend.de>
2026-05-02 18:41:02 +0200

    dryve template

--- src/app/components/projects/dryve-component/dryve-component.ts
+++ src/app/components/projects/dryve-component/dryve-component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {PageContainerComponent} from '../../../../shared/page-container-component/page-container-component';
+import {RevealDirective} from '../../../../shared/reveal-directive/reveal.directive';
+import {RouterService} from '../../../services/router.service';
 
 @Component({
   selector: 'app-dryve-component',
-  imports: [PageContainerComponent],
+  imports: [PageContainerComponent, RevealDirective],
   templateUrl: './dryve-component.html',
   styleUrl: './dryve-component.scss',
 })
-export class DryveComponent {}
+export class DryveComponent {
+  private readonly routerService = inject(RouterService);
+
+  protected navigateToMacroToMicro(): void { this.routerService.navigateToMacroToMicro(); }
+  protected navigateToWocy(): void { this.routerService.navigateToWocy(); }
+  protected navigateToSalt(): void { this.routerService.navigateToSalt(); }
+}
