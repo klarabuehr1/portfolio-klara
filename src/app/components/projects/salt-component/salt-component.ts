1e5e7486c199100757ca373220b7725da939a5f7
Klara Bühr <klara.buehr@hfg-gmuend.de>
2026-05-02 18:42:59 +0200

    salt. template

--- src/app/components/projects/salt-component/salt-component.ts
+++ src/app/components/projects/salt-component/salt-component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {PageContainerComponent} from '../../../../shared/page-container-component/page-container-component';
+import {RevealDirective} from '../../../../shared/reveal-directive/reveal.directive';
+import {RouterService} from '../../../services/router.service';
 
 @Component({
   selector: 'app-salt-component',
-  imports: [PageContainerComponent],
+  imports: [PageContainerComponent, RevealDirective],
   templateUrl: './salt-component.html',
   styleUrl: './salt-component.scss',
 })
-export class SaltComponent {}
+export class SaltComponent {
+  private readonly routerService = inject(RouterService);
+
+  protected navigateToMacroToMicro(): void { this.routerService.navigateToMacroToMicro(); }
+  protected navigateToWocy(): void { this.routerService.navigateToWocy(); }
+  protected navigateToDryve(): void { this.routerService.navigateToDryve(); }
+}
