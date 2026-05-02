13236a9777172b5ce5efdea46c5cf395a5037230
Klara Bühr <klara.buehr@hfg-gmuend.de>
2026-05-02 18:38:41 +0200

    wocy template

--- src/app/components/projects/wocy-component/wocy-component.ts
+++ src/app/components/projects/wocy-component/wocy-component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {PageContainerComponent} from '../../../../shared/page-container-component/page-container-component';
+import {RevealDirective} from '../../../../shared/reveal-directive/reveal.directive';
+import {RouterService} from '../../../services/router.service';
 
 @Component({
   selector: 'app-wocy-component',
-  imports: [PageContainerComponent],
+  imports: [PageContainerComponent, RevealDirective],
   templateUrl: './wocy-component.html',
   styleUrl: './wocy-component.scss',
 })
-export class WocyComponent {}
+export class WocyComponent {
+  private readonly routerService = inject(RouterService);
+
+  protected navigateToMacroToMicro(): void { this.routerService.navigateToMacroToMicro(); }
+  protected navigateToDryve(): void { this.routerService.navigateToDryve(); }
+  protected navigateToSalt(): void { this.routerService.navigateToSalt(); }
+}
